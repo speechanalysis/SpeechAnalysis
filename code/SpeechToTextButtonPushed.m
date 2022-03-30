365afa2b69663e81977299b49843beef7ac40c00
Αλέξανδρος Βυθούλκας <p13vyth@ionio.gr>
2022-03-30 21:37:30 +0300

    Update SpeechToTextButtonPushed.m

--- code/SpeechToTextButtonPushed.m
+++ code/SpeechToTextButtonPushed.m
@@ -1,8 +1,9 @@
 function SpeechToTextButtonPushed(app, event)
+
             file = uigetfile();
             txt = wav2vec(audioread(file),app.FsEditField.Value);
             msgbox('The transformation to text is ready','Speech To Text');
             answer = questdlg('Do you want to save the text file ?','Save File');
-               if answer == 'Yes', writematrix(txt,'sttfile.txt'); end 
+            if answer == 'Yes', writematrix(txt,'sttfile.txt'); end 
 
         end
