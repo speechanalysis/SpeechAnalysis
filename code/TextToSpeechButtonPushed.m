cde7ae40844a4a01dc08053bf558e17c3a03c0e1
Αλέξανδρος Βυθούλκας <p13vyth@ionio.gr>
2022-03-30 21:55:31 +0300

    Update TextToSpeechButtonPushed.m

--- code/TextToSpeechButtonPushed.m
+++ code/TextToSpeechButtonPushed.m
@@ -1,10 +1,16 @@
-% Button pushed function: ConvertTxtToSpeechButton
-       function ButtonPushed(app, event)
-           a = uigetfile(); %anoigei ena dialog gia na ginei upload ena file apo to file manager
-           fid = fopen(a); %anoigei to file pou epilexthke
-           txt = fscanf(fid,'%s'); %diabazei-metatrepei to txt file se symboloseira
-           pace = app.PaceSpinner.Value; %pairnei thn timh poy exoyme dwsei gia to pace mesw gui
-           fs = app.FsEditField.Value; %pairnei thn timh poy exoyme dwsei gia to fs mesw gui
-           tts(txt,'1',pace,fs); %ekteleitai h synarthsh tts gia thn metatroph tou keimenoy se omilia
-           fclose(fid); %kleinei to txt file
+% Button pushed function: TextToSpeechButton
+        function TextToSpeechButtonPushed2(app, event)
+            %a = uigetfile();
+            %fid = fopen(a);
+            %txt = fscanf(fid,'%s');
+            %formatSpec = '%s'
+            %txt = extractFileText(a);
+            txt = app.TextEditField.Value; %pairnei thn timh poy exoyme dwsei sto plaisio toy gui gia symboloseira
+            pace = app.PaceSpinner.Value; %pairnei thn timh poy exoyme dwsei gia to pace mesw gui
+            fs = app.FsEditField.Value; %pairnei thn timh poy exoyme dwsei gia to fs mesw gui
+            tts(txt,'1',pace,fs); %ekteleitai h synarthsh tts gia thn metatroph tou keimenoy se omilia
+            answer = questdlg('Do you want to save the audio file ?','Save Audio');
+            wav = tts(txt,'1',pace,fs);
+            if answer == 'Yes', audiowrite('sample.wav',wav,fs); end
+            %fclose(fid);
         end
