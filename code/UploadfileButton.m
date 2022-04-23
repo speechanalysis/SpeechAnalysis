7500aad73be2f9646ef577abcd55f31b94190395
Αλέξανδρος Βυθούλκας <p13vyth@ionio.gr>
2022-04-23 13:07:09 +0300

    Delete UploadfileButton.m

--- code/UploadfileButton.m
+++
@@ -1,9 +0,0 @@
-function UploadfileButtonPushed(app, event)
-            file = uigetfile(); %dialogbox gia na dialexei o xrhsths audio file
-            [filepath,name,ext] = fileparts(file); % returns the path name, file name, and extension about the specified file
-            if strcmp(ext,'.wav') | strcmp(ext,'.flac'); %elgxos mese strcmp ean oi symboloseira ext einai idia me thn symboloseira wav h me thn flac
-                app.file=file; %pernaei to file poy epelexe o xrhsths se mia metablhth poy tha mporoyn na blepoyn thn timh ths kai alles synarthseis toy app
-            else
-                f=errordlg('You should give the right type of file'); %alliws emfanizei ena erro message dialog me to mhnyma oti o xrhsths prepei na dwsei to swsto typo arxeioy 
-            end   
- end
