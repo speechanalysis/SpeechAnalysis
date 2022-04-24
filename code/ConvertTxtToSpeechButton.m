9d9f0a9edb4617e69b3600344dfb54e657b186c8
Αλέξανδρος Βυθούλκας <p13vyth@ionio.gr>
2022-04-24 13:45:14 +0300

    Update ConvertTxtToSpeechButton.m

--- code/ConvertTxtToSpeechButton.m
+++ code/ConvertTxtToSpeechButton.m
@@ -1,10 +1,15 @@
 % Button pushed function: ConvertTxtToSpeechButton
        function ButtonPushed(app, event)
-          a = uigetfile(); %anoigei ena dialog gia na ginei upload ena file apo to file manager
-          fid = fopen(a); %anoigei to file pou epilexthke
-          txt = fscanf(fid,'%s'); %diabazei-metatrepei to txt file se symboloseira
-          pace = app.PaceSpinner.Value; %pairnei thn timh poy exoyme dwsei gia to pace mesw gui
-          fs = app.FsEditField.Value; %pairnei thn timh poy exoyme dwsei gia to fs mesw gui
-          tts(txt,'1',pace,fs); %ekteleitai h synarthsh tts gia thn metatroph tou keimenoy se omilia
-          fclose(fid); %kleinei to txt file
+              a = uigetfile(); %anoigei ena dialog gia na ginei upload ena file apo to file manager
+              fid = fopen(a); %anoigei to file pou epilexthke
+              txt = fscanf(fid,'%s'); %diabazei-metatrepei to txt file se symboloseira
+              %txt = extractFileText(a);
+              %txt = app.TextEditField.Value;
+              pace = app.PaceSpinner.Value; %pairnei thn timh poy exoyme dwsei gia to pace mesw gui
+              fs = app.FsEditField.Value; %pairnei thn timh poy exoyme dwsei gia to fs mesw gui
+              tts(txt,'1',pace,fs); %ekteleitai h synarthsh tts gia thn metatroph tou keimenoy se omilia
+              answer = questdlg('Do you want to save the audio file ?','Save Audio');
+              wav = tts(txt,'1',pace,fs);
+              if answer == 'Yes', audiowrite('sample.wav',wav,fs); end
+              fclose(fid); %kleinei to txt file
        end
